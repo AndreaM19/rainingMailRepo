0be9ab730cdb8c6a5c6bfb8ec81dc690355c44aa
AndreaM19 <19andrea.m@gmail.com>
2014-03-28 20:01:08 +0100

    Receving Test

--- include/DB_data/db_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Database data
-$USER="root";
-$DB="au_newsletter";
-$HOST="localhost";
-$PASSWORD="andr3a";
-?>
--- include/String_messages/messages.php
+++
@@ -1,6 +0,0 @@
-<?php
-//String messages
-
-$CONFIRM_DELETE_MESSAGE_INIT="Sei sicuro di voler eliminare ";
-$CONFIRM_DELETE_MESSAGE_END=" dalla lista degli iscritti?"
-?>
